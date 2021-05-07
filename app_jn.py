18cf24fc8259487d43273acda3da113ab61494f1
Jillian Nemec <JNemec1@nuancesolutions.com>
2021-05-07 18:26:34 -0500

    commitjn

--- app_jn.py
+++ app_jn.py
@@ -1,4 +1,5 @@
 import os
+from os import name
 from flask import (
     Flask,
     render_template,
@@ -23,43 +24,35 @@ app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:"+ pw +"@localhos
 # Remove tracking modifications
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app)
+wine_db = SQLAlchemy(app)
 
-# class Pet(db.Model):
-#         __tablename__ = 'pets'
-
-#         id = db.Column(db.Integer, primary_key=True)
-#         name = db.Column(db.String(64))
-#         lat = db.Column(db.Float)
-#         lon = db.Column(db.Float)
-
-#         def __repr__(self):
-#             return '<Pet %r>' % (self.name)
-
-# create route that renders index.html template
-# @app.route("/")
-# def home():
-#     return render_template("index.html")
-
+#create route that renders index.html template
 @app.route("/")
-def index():
-    "List all routes that are available"
-    return (
-        f"Available Routes:</br>"
-        f"/wines</br>"
-    )
+def home():
+    return render_template("index.html")
 
-# # Query the database and send the jsonified results
+@app.route("/api/wine_data")
+def wine_data():
+    
+
+# Query the database and send the jsonified results
 # @app.route("/send", methods=["GET", "POST"])
 # def send():
 #     if request.method == "POST":
-#         name = request.form["petName"]
-#         lat = request.form["petLat"]
-#         lon = request.form["petLon"]
+#         year = request.form['Year']
+#         country = request.form['Country']
+#         county = request.form['County']
+#         designation = request.form['Designation']
+#         points = request.form['Points']
+#         price = request.form['Price ']
+#         province = request.form['Province']
+#         title = request.form['Title']
+#         variety = request.form['Variety']
+#         winery = request.form['Winery']
 
-#         pet = Pet(name=name, lat=lat, lon=lon)
-#         db.session.add(pet)
-#         db.session.commit()
+#         wine = Wine(year=year,title=title)
+#         wine_db.session.add(title)
+#         wine_db.session.commit()
 #         return redirect("/", code=302)
 
 #     return render_template("form.html")
@@ -90,6 +83,52 @@ def index():
 #     }]
 
 #     return jsonify(pet_data)
+
+
+# if __name__ == "__main__":
+#     app.run()
+
+# # # Query the database and send the jsonified results
+# # @app.route("/send", methods=["GET", "POST"])
+# # def send():
+# #     if request.method == "POST":
+# #         name = request.form["petName"]
+# #         lat = request.form["petLat"]
+# #         lon = request.form["petLon"]
+
+# #         pet = Pet(name=name, lat=lat, lon=lon)
+# #         db.session.add(pet)
+# #         db.session.commit()
+# #         return redirect("/", code=302)
+
+# #     return render_template("form.html")
+
+
+# # @app.route("/api/pals")
+# # def pals():
+# #     results = db.session.query(Pet.name, Pet.lat, Pet.lon).all()
+
+# #     hover_text = [result[0] for result in results]
+# #     lat = [result[1] for result in results]
+# #     lon = [result[2] for result in results]
+
+# #     pet_data = [{
+# #         "type": "scattergeo",
+# #         "locationmode": "USA-states",
+# #         "lat": lat,
+# #         "lon": lon,
+# #         "text": hover_text,
+# #         "hoverinfo": "text",
+# #         "marker": {
+# #             "size": 50,
+# #             "line": {
+# #                 "color": "rgb(8,8,8)",
+# #                 "width": 1
+# #             },
+# #         }
+# #     }]
+
+# #     return jsonify(pet_data)
 
     
 if __name__ == "__main__":
